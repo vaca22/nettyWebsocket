75291eaa3a45696fbc57d17ac9078d340d554c6c
liguanghao <liguanghao@viatomtech.com>
2022-11-17 16:44:44 +0800

    sdg5415rdtrdgdfgcvdsdfg

---
+++ src/main/resources/user_data.sql
@@ -0,0 +1,1 @@
+insert into five_chess_user (name, uid, header_url) values ('San Francisco', 'CA', 'US');
---
+++ src/main/resources/user_info.sql
@@ -0,0 +1,7 @@
+drop table if exists five_chess_user;
+
+create table five_chess_user (
+    uid varchar(16) primary key not null ,
+     name varchar(30),
+     header_url varchar(360)
+     );
